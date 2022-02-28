cb507e5f3870ef451b9afe58c4bb2d8b0a30e937
Shanawar99 <shanawar680@hotmail.com>
2022-02-28 11:55:47 +0500

    world api pubspec changes

--- flutter_application_3/lib/pages/home.dart
+++ flutter_application_3/lib/pages/home.dart
@@ -15,17 +15,37 @@ class _HomeState extends State<Home> {
     data = ModalRoute.of(context)!.settings.arguments as Map;
 
     return Scaffold(
-      body: SafeArea(
-          child: Column(
-        children: [
-          ElevatedButton.icon(
-              onPressed: () {
-                Navigator.pushNamed(context, '/location');
-              },
-              icon: const Icon(Icons.edit_location),
-              label: const Text('Edit Location'))
-        ],
-      )),
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(0.0, 100, 0, 0),
+        child: SafeArea(
+            child: Column(
+          children: [
+            ElevatedButton.icon(
+                onPressed: () {
+                  Navigator.pushNamed(context, '/location');
+                },
+                icon: const Icon(Icons.edit_location),
+                label: const Text('Edit Location')),
+            const SizedBox(height: 50),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(data['location'],
+                    style: const TextStyle(
+                        fontSize: 20,
+                        color: Colors.red,
+                        fontWeight: FontWeight.bold)),
+              ],
+            ),
+            const SizedBox(height: 10),
+            Text(data['time'],
+                style: const TextStyle(
+                    fontSize: 40,
+                    color: Colors.red,
+                    fontWeight: FontWeight.bold))
+          ],
+        )),
+      ),
     );
   }
 }
